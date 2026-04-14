d014b3af203ab28dd7bcd5cd895a9f6a1f410dd2
Susannah Young <158405300+susannah-young@users.noreply.github.com>
2026-04-13 20:37:57 -0700

    Delete 2.sql

--- 2.sql
+++
@@ -1,5 +0,0 @@
--- Goal: Identify districts that are no longer operational.
--- Method: Search for district names ending with "(non-op)" using the LIKE operator.
-SELECT name
-FROM districts
-WHERE name LIKE '%(NON-OP)';
